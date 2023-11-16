6482ef4b8ba5e5104f806f82658fc1a0505305da
cledwin <cledwin@cakeplabs.com>
2023-11-16 13:29:30 +0800

    feat: add env and dynammic route

---
+++ src/app/[locale]/[aff]/[code]/page.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Superalink – Local eSIM for Traveler",
+  description:
+    "Data packs with local rates. Buy the affordable eSIM package in Superalink.",
+  openGraph: {
+    images: "https://i.ibb.co/RY2Gz3H/Base-Thumbnail.jpg",
+  },
+};
+
+//Component
+import Navbar from "@/components/organisms/Navbar/Navbar";
+import Guide from "@/components/organisms/Guide/Guide";
+import CTA from "@/components/template/Landing/CTA";
+import Partners from "@/components/template/Landing/Partner";
+import Destination from "@/components/template/Landing/Destination";
+import Advantage from "@/components/template/Landing/Advantage";
+import Hero from "@/components/template/Landing/Hero";
+
+export default function AffLink({ params }: { params: { locale: string } }) {
+  return (
+    <>
+      <Navbar params={params} />
+      <Hero />
+      <Advantage />
+      <Destination />
+      <Partners />
+      <Guide /> 
+      <CTA />
+    </>
+  );
+}
