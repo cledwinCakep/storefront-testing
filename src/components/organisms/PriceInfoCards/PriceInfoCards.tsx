c2edfee800c887dddf86ea22938f607fede62c19
cledwin <cledwin@cakeplabs.com>
2023-11-30 13:36:38 +0800

    fix: country image

--- src/components/organisms/PriceInfoCards/PriceInfoCards.tsx
+++ src/components/organisms/PriceInfoCards/PriceInfoCards.tsx
@@ -17,36 +17,43 @@ interface PriceInfoCardsProps {
 }
 
 const i: any = {
-  CN: "china",
-  SG: "singapore",
-  MY: "malaysia",
-  TH: "thailand",
-  JP: "japan",
-  KR: "korea",
+  CN: "CN",
+  SG: "SG",
+  MY: "MY",
+  TH: "TH",
+  JP: "JP",
+  KR: "KR",
 };
 
 const PriceInfoCards = ({ data }: PriceInfoCardsProps) => {
-  const countryName = data.map((item) => i[item.code]);
-  console.log({ countryName });
+  const countryName = data.filter((item) => i[item.code]);
+
   return (
     <div className="grid w-full gap-y-4 sm:gap-x-3 sm:gap-y-2 md:w-auto md:grid-cols-4 md:gap-x-[18px] md:gap-y-6 ">
-      {data.map((data) => (
-        <Link
-          key={data.code}
-          href={`/plans/${data.code}?plan=UNLIMITED&data=500MB&duration=1`}
-        >
-          <div
-            key={data.title}
-            className="w-full rounded-lg border border-gray-800 bg-[#121417] p-4 transition-all duration-200 ease-in-out hover:border-orange-500 hover:shadow-2xl hover:shadow-orange-500/30 md:min-w-[272px]"
+      {data.map((data, index) => {
+        console.log(
+          "iss",
+          i[data.code])
+        
+        return (
+          <Link
+            key={data.code}
+            href={`/plans/${data.code}?plan=UNLIMITED&data=500MB&duration=1`}
           >
-            <PriceInfo
-              image={countryName ? data.image : `/china.png`}
+            <div
+              key={data.title}
+              className="w-full rounded-lg border border-gray-800 bg-[#121417] p-4 transition-all duration-200 ease-in-out hover:border-orange-500 hover:shadow-2xl hover:shadow-orange-500/30 md:min-w-[272px]"
+            >
+              
+              <PriceInfo
+              image={i[data.code] == data.code ? data.image :"/china.png"}
               title={data.title}
               price={data.price}
             />
-          </div>
-        </Link>
-      ))}
+            </div>
+          </Link>
+        );
+      })}
     </div>
   );
 };
