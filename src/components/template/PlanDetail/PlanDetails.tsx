a707122bb7cfe9d3538998fe86198e8cfc78a3e8
Adi Triginarsa <adi@cakeplabs.com>
2023-12-06 20:42:04 +0800

    fix: image and list countries

--- src/components/template/PlanDetail/PlanDetails.tsx
+++ src/components/template/PlanDetail/PlanDetails.tsx
@@ -35,7 +35,8 @@ const PlanDetails = ({ params }: { params: { [x: string]: string } }) => {
   const country_code = router.split("/")[2];
 
   const z: any = {
-    WW: "Global 146 Countries",
+    WW_146: "Global 146 Countries",
+    WW_54: "Global 54 Countries",
     KH: "Cambodia",
     US_CA: "United States Canada",
     AE: "United Arab Emirates",
@@ -70,7 +71,8 @@ const PlanDetails = ({ params }: { params: { [x: string]: string } }) => {
     SE: "Sweden",
     DK: "Denmark",
     AT: "Austria",
-    EU: "33 European Countries",
+    EU_42: "42 European Countries",
+    EU_33: "33 European Countries",
     ID: "Indonesia",
     VN: "Vietnam",
     HK: "Hongkong",
@@ -82,6 +84,7 @@ const PlanDetails = ({ params }: { params: { [x: string]: string } }) => {
   };
 
   const countryName = z[country_code];
+  console.log(data);
 
   return (
     <div className="sm:relative">
@@ -158,6 +161,7 @@ const PlanDetails = ({ params }: { params: { [x: string]: string } }) => {
                   value: key, // Generate a value based on the label
                 }))}
               />
+
               <RadioPlan
                 name="data"
                 title={t("planDetail_selectDataTitle")}
