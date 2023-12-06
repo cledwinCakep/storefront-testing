ed8d69a2a58950eb4d1a6cf791b0b3b5a9078c80
janaprc <jana@cakeplabs.com>
2023-12-06 16:40:36 +0800

    feat: initialize modal

--- src/components/organisms/Navbar/Navbar.tsx
+++ src/components/organisms/Navbar/Navbar.tsx
@@ -1,4 +1,5 @@
 "use client";
+import SupportedCountryModal from "@/components/template/PlanDetail/SupportedCountryModal";
 // context
 import NavbarDesktop from "./NavbarDesktop";
 import { NavbarMobileMenu } from "./NavbarMobile";
@@ -6,6 +7,7 @@ import { NavbarMobileMenu } from "./NavbarMobile";
 export const Navbar = ({ params }: { params: { locale: string } }) => {
   return (
     <div className="fixed top-0 right-0 left-0 z-50 ">
+      <SupportedCountryModal/>
       <NavbarDesktop params={params} />
       <NavbarMobileMenu params={params} />
     </div>
