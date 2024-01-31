ef66de697bc51b1b969812e302747daa94493a20
Oka Wibawa <okaa.wibawa@gmail.com>
2024-01-31 15:17:33 +0800

    fix: ubt-265 and swaps news number one and two

--- src/app/[locale]/blog/[details]/page.tsx
+++ src/app/[locale]/blog/[details]/page.tsx
@@ -13,16 +13,6 @@ const BlogDetails = ({ params }: { params: { details: string, locale:string } })
   const news = [
     {
       id: 0,
-      link: "/blog/South Korea's LG U+ Launches Innovative eSIM Promotion",
-      topic: "Hot",
-      date: "31 May 2023",
-      title: "South Korea's LG U+ Launches Innovative eSIM Promotion",
-      desc: "LG U+, a leading telecommunications enterprise in South Korea, is launching a marketing campaign featuring a complimentary T-Money card for all travelers in South Korea who obtain a prepaid eSIM via its official web portal (https://www.lguplus.com/ib-roaming/pc/eng). The eSIM is a downloadable service that enables dual numbering without the hassle of replacing the current SIM card. LG U+'s eSIM, a data product of unparalleled convenience, can be swiftly utilized in Korea by scanning the QR code delivered via email upon purchase, eliminating the need to visit physical retail outlets such as airport counters. Customers are presented with the opportunity to choose a plan duration ranging from a minimum of 5 days to a maximum of 60 days, with unlimited data usage available for the entire plan period. Moreover, customers have the flexibility to extend the term at any point during its usage via the homepage. Should the period reach its conclusion, they retain the option to apply for an extension once more within three days. Starting May 20, 2023, LG U+ is implementing a special campaign where each customer purchasing an eSIM through the website will receive a T-Money card. This transportation card is valid across all public transit systems in Korea and is an essential commodity for travel within the nation. This special deal grants customers the privilege of a 10% discount on their purchase, saving them the inconvenience of acquiring a separate T-Money card. The T-Money card can be obtained from the LG U+ Customer Center at the airport by presenting the email received after the website purchase.",
-      image: "/assets/korean-esim-news.jpeg",
-      author: "The Fast Mode",
-    },
-    {
-      id: 1,
       link: "/blog/South Korea introduces 2 numbers in 1 phone service with eSIM",
       topic: "Trending",
       date: "31 August 2022",
@@ -30,6 +20,16 @@ const BlogDetails = ({ params }: { params: { details: string, locale:string } })
       desc: "South Korea will begin embedded subscriber identity module, known as eSIM, services from Thursday, allowing Koreans to have two different numbers in one mobile device, the Ministry of Science and ICT said Wednesday. The eSIM service, an upgrade from the universal subscriber identity module, is based on downloading the electronic profile of subscribers to each device. Therefore, it does not require a physical USIM card change. The ministry said Koreans can now choose between the two types of SIM cards, but eSIM offers faster and more convenient services as the entire purchasing process can be done online. The price of getting a new eSIM is 2,750 won ($2), much cheaper than buying a USIM at the minimum cost of 7,700 won, it added. With the introduction of eSIM services, users can have two different phone numbers in one device -- one for USIM and another for eSIM. According to the ministry, eSIM needs to be installed in smartphones from the production stage. Currently, Samsung Electronics' Galaxy Z Fold4 and Z Flip4, along with Apple's smartphone lineups beyond the iPhone XS, which hit the domestic market in 2018, have built-in eSIM services. The ministry said it will cooperate with phone manufacturers and telecommunication operators to expand the list of mobile devices that can provide eSIM services. The country's three major mobile carriers -- SK Telecom, KT and LG Uplus -- are holding promotions to offer eSIM free of charge for customers who buy eSIM services for the first time through December. According to an intelligence report from GSMA, a global mobile communications organization, commercial eSIM services had been launched in at least 88 countries around the world as of June this year.",
       image: "/assets/introduces.jpeg",
       author: "The Korea Herald",
+    },
+    {
+      id: 1,
+      link: "/blog/South Korea's LG U+ Launches Innovative eSIM Promotion",
+      topic: "Hot",
+      date: "31 May 2023",
+      title: "South Korea's LG U+ Launches Innovative eSIM Promotion",
+      desc: "LG U+, a leading telecommunications enterprise in South Korea, is launching a marketing campaign featuring a complimentary T-Money card for all travelers in South Korea who obtain a prepaid eSIM via its official web portal (https://www.lguplus.com/ib-roaming/pc/eng). The eSIM is a downloadable service that enables dual numbering without the hassle of replacing the current SIM card. LG U+'s eSIM, a data product of unparalleled convenience, can be swiftly utilized in Korea by scanning the QR code delivered via email upon purchase, eliminating the need to visit physical retail outlets such as airport counters. Customers are presented with the opportunity to choose a plan duration ranging from a minimum of 5 days to a maximum of 60 days, with unlimited data usage available for the entire plan period. Moreover, customers have the flexibility to extend the term at any point during its usage via the homepage. Should the period reach its conclusion, they retain the option to apply for an extension once more within three days. Starting May 20, 2023, LG U+ is implementing a special campaign where each customer purchasing an eSIM through the website will receive a T-Money card. This transportation card is valid across all public transit systems in Korea and is an essential commodity for travel within the nation. This special deal grants customers the privilege of a 10% discount on their purchase, saving them the inconvenience of acquiring a separate T-Money card. The T-Money card can be obtained from the LG U+ Customer Center at the airport by presenting the email received after the website purchase.",
+      image: "/assets/korean-esim-news.jpeg",
+      author: "The Fast Mode",
     },
     {
       id: 2,
